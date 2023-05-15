fa11256859dd28926ac473a1e845ef26ef04a544
kavya217 <133665481+kavya217@users.noreply.github.com>
2023-05-15 18:34:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function setup (){
+    canvas = createCanvas(400,400);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+function draw(){
+    strokeWeight(7);
+    stroke('blue');
+    if (mouseIsPressed){
+        line(pmouseX , pmouseY , mouseX , mouseY);
+    }
+}
+function classifyCanvas(){
+    classifier.classify(canvas, gotResults);
+}
+function gotResults(error,results){
+    if (error){
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML = 'Label:' + results[0].label;
+    document.getElementById('confidence').innerHTML = 'Confidence:' + Math.round(results[0].confidence*100) + '%';
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
+
+function clearCanvas(){
+    background("white");
+}
